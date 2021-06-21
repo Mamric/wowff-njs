3bfeb6aa8cea705e6b7e7b5344bb188203938302
Nick Miller Desktop <nmiller3@go.pasadena.edu>
2021-06-20 17:10:31 -0700

    updated background color and moved footer to bottom of screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,22 @@ import { Header } from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="bg-gray-800 text-gray-400">
-      <Head>
-        <title>WoW Friend Finder</title>
-        <meta
-          name="description"
-          content="The Most Powerful friend-finding tool for World of Warcraft"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <TestimonyLister />
-      <Footer />
+    <div className="h-100 bg-gray-800">
+      <div className="flex flex-col bg-gray-800 text-gray-400 h-screen">
+        <Head>
+          <title>WoW Friend Finder</title>
+          <meta
+            name="description"
+            content="The Most Powerful friend-finding tool for World of Warcraft"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <div className="flex-grow">
+          <TestimonyLister />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
